f3a14d967d39a0b8b8cad5f74686fc6900759a36
DashSoma <daobandoso@est.utn.ac.cr>
2026-02-07 17:48:38 -0600

    Borrar
    Archivitos borrados

---
+++ LabMoviles02/app/(tabs)/button.tsx
@@ -0,0 +1,20 @@
+import { View, Text, Button, Alert } from 'react-native'
+import React from 'react'
+
+export default function button() {
+  return (
+    <View>
+      <Text>button</Text>
+      <Button
+        title="Presióname"
+        color="#1e90ff"
+        onPress={() =>
+          Alert.alert(
+            'Mensaje',
+            'El botón fue presionado correctamente'
+          )
+        }
+      />
+    </View>
+  )
+}
