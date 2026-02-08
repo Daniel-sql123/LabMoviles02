445d2254eace5538e5e367180bdc802f18c7b519
Zuly22 <zurojasco@est.utn.ac.cr>
2026-02-07 19:11:18 -0600

    A como estaba

--- LabMoviles02/app/_layout.tsx
+++ LabMoviles02/app/_layout.tsx
@@ -1,7 +1,7 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
